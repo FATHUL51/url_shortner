303366388185c96f802cc8a5d3fd09c0582bdf5b
Sk Fathul Islam <178652788+FATHUL51@users.noreply.github.com>
2025-01-30 11:58:37 +0530

    project done

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,12 @@ const device = require("express-device");
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow requests from this origin
+    credentials: true,
+  })
+);
 app.use(device.capture());
 app.use(cookieParser());
 app.use("/api/user", userRoute);
